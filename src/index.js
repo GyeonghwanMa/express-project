35eceffea02154d85332afffd095ca21c013323f
Gyeonghwan <ghma1213@gmail.com>
2021-12-30 14:55:29 +0900

    주석 추가

--- src/index.js
+++ src/index.js
@@ -12,20 +12,23 @@ const app = express();
 
 const server = async () => {
     try {
+        // DB연결
         await connect(MONGO_URI);
 
         // json을 자바스크립트로 변경
         app.use(json());
 
+        // Hello World!
         app.get('/', (req, res) => {
             res.send('Hello World!')
         })
         
+        // 서버 포트 설정
         app.listen(PORT, async () => {
             console.log(`server listening on port ${PORT}`);
         });
 
-        
+        // 아폴로 서버 설정
         const apolloServer = new ApolloServer({
             schema,
             playground: true,
