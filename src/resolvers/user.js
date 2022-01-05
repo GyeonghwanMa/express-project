96769fc9bcf87e73971b3b35140b21c21ff16197
Gyeonghwan <ghma1213@gmail.com>
2022-01-05 15:01:30 +0900

    console.log 추가

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,8 +1,13 @@
 export const userResolvers = {
     Query: {
-        
+        async getUser() {
+            console.log("getUser 실행!")
+        }
     },
     Mutation: {
-
+        async insertUser({email, password, name}) {
+            console.log("insertUser 실행!")
+            
+        }
     },
 }
